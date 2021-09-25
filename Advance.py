175b288d36cc5e674e5a687b9763de928c6e31a9
Ghaida'a Shtayeh <79005401+GhaidaaShtayeh@users.noreply.github.com>
2021-09-25 16:53:38 +0300

    Updating and Do some design changes

--- Advance.py
+++ Advance.py
@@ -1,3 +1,4 @@
+##################################################################################################################
 instances = []
 count = 0 
 class connection(object):
@@ -8,17 +9,18 @@ class connection(object):
     # it's same implemantition for getInstance but we have __new__ method in python 
     def __new__(cls):
         global count
-     #   global _count
+         #global _count
         # Check if an instance exists:
         if count < 3:
             count += 1
+            print(count , " . " , " new instance created")
             if not cls._instance :
             # Create new instance:
                 cls._instance = object.__new__(cls)
-                instances.append(cls)
+                instances.append(cls._instance)
             return cls._instance
         else :
-            print("Too many instances were created")
+            print("* Too many instances were created")
         
     
     def release(self):
@@ -28,7 +30,7 @@ class connection(object):
     
 def getCurrentConnections():
         return instances
-    
+##############################################################################################################
 class http(connection):
     def send(self):
         print("sending my message via http" )
@@ -36,67 +38,96 @@ class http(connection):
         #do smthing unique for this class
         # in this or other abstract method
         # also pearent method can be called with help of super
-    def release(self) : 
-        print(self._instance)
-        del connection._instance
+    def release(self) :
+        global count
+        instances.remove(self)
+        self._instance = None
+        count = count - 1
         print("died")
-
+##############################################################################################################
 class scp(connection):
     def send(self):
         print("sending my message via scp ")
         pass
         
     def release(self) :
-        del connection._instance
-        
+        global count 
+        count = count -1 
+        instances.remove(self)
+        self._instance = None
         print("died")
+###############################################################################################################
 class ssh(connection):
     def send(self):
         print("sending my message via ssh" )
         pass
     
     def release(self) :
-        del connection._instance
+        global count 
+        count = count - 1 
+        instances.remove(self)
+        self._instance = None
         print("died")
+###############################################################################################################               
 class telnet(connection):
     def send(self):
         print("sending my message via telnet ")
         pass
 
     def release(self) :
-        del connection._instance
-        print("died")    
+        global count 
+        count = count -1
+        instances.remove(self)
+        self._instance = None
+        print("died")
+##############################################################################################################
 class ftp(connection):
     def send(self):
         print("sending my message via ftp" )
         pass
-        #do smthing unique for this class
-        # in this or other abstract method
-        # also pearent method can be called with help of super
+
     def release(self) :
-        del connection._instance
+        global count 
+        count = count - 1
+        instances.remove(self)
+        self._instance = None
         print("died")
 
-
-
+########################################### MAIN FUNCTION ############################################################
 def main():
-    a1 = http()
-    a2 = http()
-    b1 = scp()
-    b2 = scp()
-    c1 = telnet()
-    if a1 == a2 :
-        print("true")     
-    a1.release()
-    b1.send()
-    if a1 == a2 :
+    print("-------------------------MAIN AND SOME TEST CASES-------------------------------")
+    http1 = http()
+    http2 = http()
+    scp1 = scp()
+    # no more three connections
+    print("* create new connection - more than three : ")
+    telnet1 = telnet()
+    print("--------------------------------------------------------")
+    #First test case :
+    print ( "* http1 == http2 ??? ")
+    if http1 == http2 :
+        print("true") 
+    print("--------------------------------------------------------")
+    # send function
+    print( "* calling send() method ")
+    scp1.send()
+    print("--------------------------------------------------------")
+    #second test case
+    print("b1 == a1 ??? ")
+    if scp1 == http1:
         print("true")
-    if b1 == a1:
-        print("truuue")
-    else : print("not")
-    print(getCurrentConnections())
+    else : print("False")
+    print("--------------------------------------------------------")
+    # object delete
+    print("* Delete http object and create new one : ")
+    http1.release()
+    telnet1 = telnet()
+    print("--------------------------------------------------------")
+    #print list of current connections 
+    print( "* list of our current connections : ")
+    x = getCurrentConnections()
+    print("["," , ".join([str(i.__class__.__name__ ) for i in x]) , "]")
 
-
-
+####################################################################################################################
 if __name__ == "__main__":
     main()
